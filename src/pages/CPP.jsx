0ff88a74dc4c689ce17b3bfdfdf23a769c86c09c
ImNotDarren <sliu178@binghamton.edu>
2023-01-19 02:57:33 -0500

    updated CPP model (almost finished)

--- src/pages/CPP.jsx
+++ src/pages/CPP.jsx
@@ -6,15 +6,34 @@ import { connect } from 'react-redux';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 import Button from '@mui/material/Button';
 
 function CPP(props) {
 
     const [url, setUrl] = useState('')
 
+    const [predictedPrice, setPredictedPrice] = useState(-1)
+    const [price, setPrice] = useState(-1)
+
     const [winWidth, setWinWidth] = useState(document.querySelector('body').offsetWidth)
 
+    const currYear = new Date().getFullYear()
+    const [year, setYear] = useState(currYear+1)
+    const [yearSelectList, setYearSelectList] = useState([])
+
     useEffect(() => {
+
+        let tmp = []
+        for (let i=currYear+1; i<currYear+11; i++) {
+            tmp.push(i)
+        }
+
+        setYearSelectList(tmp)
 
         const handleResize = e => {
             if (e.target.innerWidth <= 560) {
@@ -41,9 +60,16 @@ function CPP(props) {
     const handleSubmit = () => {
         // check if the url is from cars.com
         const urlpre = 'https://www.cars.com/vehicledetail/'
-        if (url.startsWith(urlpre, 0)){
-            console.log('correct url')
+        if (url.startsWith(urlpre, 0)) {
+            let new_url = url.slice(35, -1)
             // make a request to backend
+            fetch('http://flask-be.eba-yxg2mgcp.us-east-1.elasticbeanstalk.com/cpp/' + new_url + '/' + year)
+                .then(res => res.json())
+                .then(res => {
+                    setPredictedPrice(res['predictedPrice'])
+                    setPrice(res['price'])
+                    console.log(res['year'])
+                })
         } else {
             message.error('Wrong URL!')
         }
@@ -53,17 +79,50 @@ function CPP(props) {
         setUrl('')
     }
 
+    const handleYear = e => {
+        console.log(e.target.value)
+        setYear(e.target.value)
+    }
+
 
     return (
         <>
             <div className='cpp_content'>
-                <div className="cpp_title">Paste your cars.com url here:</div>
-                <TextField id="outlined-basic" label="cars.com url" variant="outlined" onChange={getUrl} sx={{width: winWidth <= 560 ? '100%' : '500px', marginTop: '10px'}} value={url}></TextField>
-                <div className="cpp_btns">
-                    <Button variant='outlined' onClick={handleSubmit} sx={{marginRight: '10px', width: winWidth <= 560 ? '100%' : '100px'}}>submit</Button>
-                    <Button variant='outlined' color='error' onClick={handleClear} sx={{width: winWidth <= 560 ? '100%' : '100px'}}>clear</Button>
+                <div className="cpp_title">Paste your cars.com url and predicted year here:</div>
+                <div className="input">
+                    <TextField id="outlined-basic" label="cars.com url" variant="outlined" onChange={getUrl} sx={{ width: winWidth <= 560 ? '100%' : '500px' }} value={url}></TextField>
+                    <FormControl sx={{marginLeft: '10px', width: '120px'}}>
+                        <InputLabel id="select-year">Year</InputLabel>
+                        <Select
+                            labelId="select-year"
+                            id="select-year-select"
+                            value={year}
+                            label="Year"
+                            onChange={handleYear}
+                        >
+                            {/* <MenuItem value={10}></MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem> */}
+
+                            {
+                                yearSelectList.map((y) => (
+                                    <MenuItem value={y} key={y}>{y}</MenuItem>
+                                ))
+                            }
+                        </Select>
+                    </FormControl>
                 </div>
-                
+
+                <div className="cpp_btns">
+                    <Button variant='outlined' onClick={handleSubmit} sx={{ marginRight: '10px', width: winWidth <= 560 ? '100%' : '100px' }}>submit</Button>
+                    <Button variant='outlined' color='error' onClick={handleClear} sx={{ width: winWidth <= 560 ? '100%' : '100px' }}>clear</Button>
+                </div>
+
+                <div className="cpp_prediction" hidden={price === -1 ? true : false}>
+                    price: ${price}
+                    predicted price: ${predictedPrice}
+                </div>
+
             </div>
         </>
     )
@@ -72,7 +131,8 @@ function CPP(props) {
 const mapStateToProps = (state) => {
     return {
         uid: state.uid,
-        language: state.language
+        language: state.language,
+        python_server: state.python_server
     }
 }
 
